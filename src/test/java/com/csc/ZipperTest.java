271c621a4591ca2be0dda6c07695a04ea17967ea
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-10-06 10:31:05 -0500

    Add skeleton for the lab

---
+++ src/test/java/com/csc/ZipperTest.java
@@ -0,0 +1,21 @@
+package com.csc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ZipperTest {
+
+  Zipper zipper;
+
+  @BeforeEach
+  void setUp() {
+    zipper = new Zipper();
+  }
+
+  @Test
+  void itWorks() {
+    assertEquals(true, true);
+  }
+}
